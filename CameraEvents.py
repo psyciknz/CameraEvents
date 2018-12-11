71fa39af98ae4eb7569a93d5651afb936710f639
psyciknz <github@andc.co.nz>
2018-12-11 19:44:58 +1300

    Moved functions into DahuaDevice, added paho, start adding channel names

--- CameraEvents.py
+++ CameraEvents.py
@@ -9,15 +9,30 @@ Author: PsycikNZ
 REQUIREMENTS = ['pycurl>=7']
 
 import threading
+import requests
 import logging
 import os
 import socket
 import pycurl
 import time
+import paho.mqtt.client as paho   # pip install paho-mqtt
+
+mqttc = paho.Client("CameraEvents", clean_session=True)
 
 _LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
 
-URL_TEMPLATE = "{protocol}://{host}:{port}/cgi-bin/eventManager.cgi?action=attach&channel=1&codes=%5B{events}%5D"
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+_LOGGER.addHandler(ch)
+
+#URL_TEMPLATE = "{protocol}://{host}:{port}/cgi-bin/eventManager.cgi?action=attach&channel=0&codes=%5B{events}%5D"
+#CHANNEL_TEMPLATE = "{protocol}://{host}:{port}/cgi-bin/configManager.cgi?action=getConfig&name=ChannelTitle"
 
 #CONFIG_SCHEMA = vol.Schema({
 #    DOMAIN:
@@ -53,26 +68,64 @@ def setup( config):
 
     return True
 
-class config():
-    def __init__(self):
-        self.Name = "Ludlam"
-        self.Master = True
-        self.Protocol = "http"
-        self.Host = "192.168.10.64"
-        self.Port = "80"
-        self.Events = "MotionDetect"
-        self.User = "Remote"
-        self.Password = "Dragon25"
+#class config():
+#    def __init__(self):
+#        self.Name = "NVR"
+#        self.Master = True
+#        self.Protocol = "http"
+#        self.Host = "192.168.10.64"
+#        self.Port = "80"
+#        self.Events = "VideoMotion,CrossLineDetection,AlarmLocal,VideoLoss,VideoBlind"
+#        self.User = "Remote"
+#        self.Password = "Dragon25"
+
+    
+        
+
 
 class DahuaDevice():
-    def __init__(self, master, name, url, channels):
-        self.Master = master
+    URL_TEMPLATE = "{protocol}://{host}:{port}/cgi-bin/eventManager.cgi?action=attach&channel=0&codes=%5B{events}%5D"
+    CHANNEL_TEMPLATE = "{protocol}://{host}:{port}/cgi-bin/configManager.cgi?action=getConfig&name=ChannelTitle"
+
+    def __init__(self, name, device_cfg):
         self.Name = name
-        self.Url = url
-        self.Channels = channels
         self.CurlObj = None
         self.Connected = None
         self.Reconnect = None
+        self.user = device_cfg.get("user")
+        self.password = device_cfg.get("pass")
+
+        self.url = self.URL_TEMPLATE.format(
+            protocol=device_cfg.get("protocol"),
+            host=device_cfg.get("host"),
+            port=device_cfg.get("port"),
+            events=device_cfg.get("events")
+        )
+        self.isNVR = False
+        try:
+            self.isNVR = device_cfg.get("isNVR")
+
+            if self.isNVR:
+                self.channelurl  = self.CHANNEL_TEMPLATE.format(
+                    protocol=device_cfg.get("protocol"),
+                    host=device_cfg.get("host"),
+                    port=device_cfg.get("port")
+                )
+                # get channel names here
+                
+                response = requests.get(self.channelurl,auth=requests.auth.HTTPDigestAuth(self.user,self.password))
+                print response.text
+                #table.ChannelTitle[0].Name=Garage
+                #table.ChannelTitle[1].Name=Backyard
+                #table.ChannelTitle[2].Name=Frontdoor
+                #table.ChannelTitle[3].Name=Backdoor
+
+        except Exception,e:
+            _LOGGER.debug("Device " + name + " is not an NVR: " + str(e))
+            _LOGGER.debug("Device " + name + " is not an NVR")
+        
+    
+
 
     def OnConnect(self):
         _LOGGER.debug("[{0}] OnConnect()".format(self.Name))
@@ -103,7 +156,10 @@ class DahuaDevice():
             if Alarm["index"] in self.Channels:
                 Alarm["channel"] = self.Channels[Alarm["index"]]
 
-            
+            _LOGGER.info("dahua_event_received: "+  Alarm["name"] + " Index: " + Alarm["index"])
+            mqttc.connect("mqtt.andc.nz", int(1883), 60)
+            mqttc.publish("CameraEventsPy/" + Alarm["index"] + "/" + Alarm["name"])
+            mqttc.disconnect()
             #self.hass.bus.fire("dahua_event_received", Alarm)
 
 class DahuaEventThread(threading.Thread):
@@ -115,49 +171,30 @@ class DahuaEventThread(threading.Thread):
     NumCurlObjs = 0
 	
 
-    def __init__(self,  config):
+    def __init__(self,  cameras):
         """Construct a thread listening for events."""
 
-        #for device_cfg in config:
-        #url = URL_TEMPLATE.format(
-        #    protocol=device_cfg.get("protocol"),
-        #    host=device_cfg.get("host"),
-        #    port=device_cfg.get("port"),
-        #    events=device_cfg.get("events")
-        #)
-        url = URL_TEMPLATE.format(
-            protocol=config.Protocol,
-            host=config.Host,
-            port=config.Port,
-            events=config.Events
-        )
-        #channels = device_cfg.get("channels")
-        #channels_dict = {}
-        #if channels is not None:
-        #    for channel in channels:
-        #        channels_dict[channel.get("number")] = channel.get("name")
-        channels_dict = {}
-        channels_dict[1] = config.Name
+        for device_cfg in cameras:
 
-        device = DahuaDevice(self, config.Name, url, channels_dict)
-        self.Devices.append(device)
+            device = DahuaDevice(device_cfg.get("name"), device_cfg)
+            self.Devices.append(device)
 
-        CurlObj = pycurl.Curl()
-        device.CurlObj = CurlObj
+            CurlObj = pycurl.Curl()
+            device.CurlObj = CurlObj
 
-        CurlObj.setopt(pycurl.URL, url)
-        CurlObj.setopt(pycurl.CONNECTTIMEOUT, 30)
-        CurlObj.setopt(pycurl.TCP_KEEPALIVE, 1)
-        CurlObj.setopt(pycurl.TCP_KEEPIDLE, 30)
-        CurlObj.setopt(pycurl.TCP_KEEPINTVL, 15)
-        CurlObj.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
-        CurlObj.setopt(pycurl.USERPWD, "%s:%s" % (config.User, config.Password))
-        CurlObj.setopt(pycurl.WRITEFUNCTION, device.OnReceive)
+            CurlObj.setopt(pycurl.URL, device.url)
+            CurlObj.setopt(pycurl.CONNECTTIMEOUT, 30)
+            CurlObj.setopt(pycurl.TCP_KEEPALIVE, 1)
+            CurlObj.setopt(pycurl.TCP_KEEPIDLE, 30)
+            CurlObj.setopt(pycurl.TCP_KEEPINTVL, 15)
+            CurlObj.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+            CurlObj.setopt(pycurl.USERPWD, "%s:%s" % (device.user, device.password))
+            CurlObj.setopt(pycurl.WRITEFUNCTION, device.OnReceive)
 
-        self.CurlMultiObj.add_handle(CurlObj)
-        self.NumCurlObjs += 1
+            self.CurlMultiObj.add_handle(CurlObj)
+            self.NumCurlObjs += 1
 
-        _LOGGER.debug("Added Dahua device at: %s", url)
+            _LOGGER.debug("Added Dahua device at: %s", device.url)
 
         threading.Thread.__init__(self)
         self.stopped = threading.Event() 
@@ -182,6 +219,7 @@ class DahuaEventThread(threading.Thread):
                 for CurlObj in Success:
                     DahuaDevice = next(iter(filter(lambda x: x.CurlObj == CurlObj, self.Devices)), None)
                     if DahuaDevice.Reconnect:
+                        _LOGGER.debug("Dahua Reconnect: %s", DahuaDevice.Name)
                         continue
 
                     DahuaDevice.OnDisconnect("Success")
@@ -203,9 +241,33 @@ class DahuaEventThread(threading.Thread):
             #if Ret != pycurl.E_CALL_MULTI_PERFORM: break
 
 if __name__ == '__main__':
-    _config = config()
+    #_config = config()
+    CAMERAS = [
+        {
+            "host": "192.168.10.64",
+            "protocol": "http",
+            "name" : "NVR",
+            "isNVR": True,
+            "port": 80,
+            "user": "Remote",
+            "pass": "Dragon25",
+            "events": "VideoMotion,CrossLineDetection,AlarmLocal,VideoLoss,VideoBlind"
+        }
+    ]
+        #,
+        #{
+        #    "host": "192.168.10.65",
+        #    "protocol": "http",
+        #    "name": "Roof",
+        #    "port": 80,
+        #    "user": "USER",
+        #    "pass": "PASSWORD",
+        #    "events": "CrossLineDetection"
+        #}
+    #]
+    #_config.readConfig(CAMERAS)
 
-    dahua_event = DahuaEventThread(_config)
+    dahua_event = DahuaEventThread(CAMERAS)
 
     dahua_event.start()
 
