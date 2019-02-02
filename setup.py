1d822d29529c4272e118821ee8fbeb00b74372e9
psyciknz <github@andc.co.nz>
2019-02-02 18:51:30 +1300

    Adding more components needed for Travis

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CameraEvents',
+    packages=['CameraEvents'],
+    version='0.1.1',
+    description='Camera Events project for Dahua NVR alerting',
+    url='https://github.com/psyciknz/CameraEvents',    
+)
